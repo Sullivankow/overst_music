9fb3fab52ab78a38fe8c5cca3823b995f892097c
SunDev <sundly@live.fr>
2025-03-23 14:17:06 +0100

    Page Navbar ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,57 @@
-import { Menu } from "lucide-react"
+import { Menu } from "lucide-react";
+import logo from "../assets/logo_b.svg";
 
 const Navbar = () => {
   return (
     <div className="navbar fixed top-0 left-0 right-0 z-50 bg-transparent text-white">
-      {/* Nom de l'artiste */}
-      <div className="flex-1 px-4">
-        <a className="text-2xl font-bold">Øverst</a>
+      
+      {/* Conteneur Flex principal */}
+      <div className="flex items-center justify-between w-full px-4">
+        
+        {/* Nom de l'artiste à gauche */}
+        <div className="flex-1">
+          <a className="text-2xl font-bold">Øverst</a>
+        </div>
+
+        {/* Logo centré */}
+        <div className="flex justify-center flex-1">
+          <img src={logo} alt="Logo Øverst" className="h-16 w-16 object-contain" />
+        </div>
+
+        {/* Menus (Desktop et Mobile) à droite */}
+        <div className="flex-1 flex justify-end items-center space-x-4">
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex">
+            <ul className="menu menu-horizontal space-x-4">
+              <li><a href="#home">Accueil</a></li>
+              <li><a href="#about">À propos</a></li>
+              <li><a href="#music">Musique</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
           </div>
-          
-           {/* Ton logo */}
-        
-         <img src="/assets/logo.PNG" alt="Logo Øverst" className="h-12 w-12 object-contain" />
-        
 
-      {/* Desktop Menu */}
-      <div className="hidden md:flex px-4">
-        <ul className="menu menu-horizontal">
-          <li><a href="#home">Accueil</a></li>
-          <li><a href="#about">À propos</a></li>
-          <li><a href="#music">Musique</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+          {/* Mobile Dropdown Menu */}
+          <div className="dropdown dropdown-end md:hidden">
+            <button tabIndex={0} className="btn btn-square btn-ghost">
+              <Menu className="w-6 h-6" />
+            </button>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white"
+            >
+              <li><a href="#home">Accueil</a></li>
+              <li><a href="#about">À propos</a></li>
+              <li><a href="#music">Musique</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+
+        </div>
       </div>
 
-      {/* Mobile Dropdown Menu */}
-      <div className="dropdown dropdown-end md:hidden px-4">
-        <button tabIndex={0} className="btn btn-square btn-ghost">
-          <Menu className="w-6 h-6" />
-        </button>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white"
-        >
-          <li><a href="#home">Accueil</a></li>
-          <li><a href="#about">À propos</a></li>
-          <li><a href="#music">Musique</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
     </div>
   );
 };
 
-export default Navbar
+export default Navbar;
