5c5ca781e2c4e5093312d4494051c0d817de97f4
SunDev <sundly@live.fr>
2025-03-23 17:03:35 +0100

    Add galerie OK

--- src/components/Galerie.tsx
+++ src/components/Galerie.tsx
@@ -16,70 +16,58 @@ function Galerie() {
   }, []);
 
   return (
-    <section id="galerie" className="w-full min-h-screen bg-white text-black py-16">
+    <section id="galerie" className="w-full min-h-screen bg-black text-white py-16 items-center">
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-4xl font-bold mb-8">Ma galerie</h2>
-        <p className="text-lg text-gray-600 mb-8">
+        <p className="text-lg text-white-600 mb-8">
           Découvrez une sélection de photos et vidéos.
         </p>
 
         {/* Carrousel */}
         <div className="carousel rounded-box flex overflow-hidden">
+          
           <div className="carousel-item">
             <img
-              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-              alt="Image 1"
-              className="object-cover w-full h-full"
+              src="src\assets\overst2.jpg"
+              alt="Overst"
+              className="object-cover w-[300px] h-[300px] rounded"
             />
           </div>
           <div className="carousel-item">
             <img
-              src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-              alt="Image 2"
-              className="object-cover w-full h-full"
+              src="src\assets\overstDj2.jpg"
+              alt="Overst"
+              className="object-cover w-[300px] h-[300px] rounded"
             />
           </div>
           <div className="carousel-item">
             <img
-              src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-              alt="Image 3"
-              className="object-cover w-full h-full"
+              src="src\assets\overstDj3.jpg"
+              alt="Overst"
+              className="object-cover w-[300px] h-[300px] rounded"
             />
           </div>
           <div className="carousel-item">
             <img
-              src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-              alt="Image 4"
-              className="object-cover w-full h-full"
+              src="src\assets\overstDj4.jpg"
+              alt="Overst"
+              className="object-cover w-[300px] h-[300px] rounded"
             />
           </div>
           <div className="carousel-item">
             <img
-              src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-              alt="Image 5"
-              className="object-cover w-full h-full"
+              src="src\assets\overstDj5.jpg"
+              alt="Overst"
+              className="object-cover w-[300px] h-[300px] rounded"
             />
           </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-              alt="Image 6"
-              className="object-cover w-full h-full"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-              alt="Image 7"
-              className="object-cover w-full h-full"
-            />
-          </div>
+         
         </div>
 
         {/* Navigation buttons */}
         <div className="flex justify-between mt-8">
-          <button className="btn btn-primary carousel-prev">Précédent</button>
-          <button className="btn btn-primary carousel-next">Suivant</button>
+          <button className="btn bg-white text-black carousel-prev">Précédent</button>
+          <button className="btn bg-white text-black carousel-next">Suivant</button>
         </div>
       </div>
     </section>
